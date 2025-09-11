e16d1edf0299ca6075eeb96ca60a8bd78341357d
Light-Yagami-7 <140303675+Light-Yagami-7@users.noreply.github.com>
2025-09-11 22:29:23 +0530

    Delete pkg directory

--- pkg/mod/fyne.io/fyne/v2@v2.6.1/cmd/fyne/internal/commands/install_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package commands
-
-func postInstall(i *Installer) error {
-	return nil
-}
--- pkg/mod/fyne.io/fyne/v2@v2.6.1/internal/driver/glfw/menu_other_test.go
+++
@@ -1,6 +0,0 @@
-//go:build !no_glfw && (!darwin || no_native_menus) && !mobile
-
-package glfw
-
-func initMainMenu() {
-}
